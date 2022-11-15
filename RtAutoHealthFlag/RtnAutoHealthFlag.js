f02c059673a1b923dd3eeb7d844d54868a7fd87e
Nikki <nikecoes@gmail.com>
2022-11-15 21:42:31 +0100

    Added previuously developed scripts

---
+++ RtAutoHealthFlag/RtnAutoHealthFlag.js
@@ -0,0 +1,73 @@
+on('change:graphic', async function(obj) {
+
+  const character = getObj('character', obj.get('_represents'));
+  const type = (character.get('controlledby') === '') ? 'Monster' : 'Player';
+
+  if (obj.get('bar1_max') === '') {
+
+      const hpAttributeVal = getAttrByName(character.id, 'hp', 'max');
+      
+      obj.set('bar1_value', hpAttributeVal);
+      obj.set('bar1_max', hpAttributeVal);
+
+    }
+   
+    if (obj.get('bar1_value') <= obj.get('bar1_max') / 2 
+        && type === 'Monster') {
+
+
+      if (obj.get('status_broken-heart') === false) {
+
+        obj.set({
+
+          'status_broken-heart': true
+  
+        });
+
+        await sendChat('', `/desc ${obj.get('name')} is bloodied!`);
+
+      }
+
+    } else {
+
+        obj.set({
+
+            'status_broken-heart': false
+
+        });
+
+    }
+
+    if (obj.get('bar1_value') <= 0) {
+
+      if (type === 'Monster') {
+
+        obj.set({
+          'status_dead': true,
+          'status_broken-heart': false
+       });
+
+       await sendChat('', `/desc ${obj.get('name')} is dead!`);
+
+      } else {
+
+        obj.set({
+
+          'status_death-zone': true,
+          'status_broken-heart': false
+
+        });
+
+        await sendChat('', `/desc ${obj.get('name')} is downed!`);
+
+      }
+      
+    } else {
+
+      obj.set({
+        'status_dead': false
+      });
+
+    }
+
+});
---
+++ RtCombatFramework/RtnRoundFramework.js
@@ -0,0 +1,132 @@
+on('ready', () => {
+    
+    log('RT: RoundFramework ready!');
+
+    if (!state.rtnEncounter) {
+
+        state.rtnEncounter = {
+
+            combatRound: 1,
+            turn: null,
+            numOfCombatants: 0,
+            trackingActive: false
+
+        };
+    }
+
+    let turnOrder = JSON.parse(Campaign().get('turnorder'));
+
+    on('chat:message', async msg => {
+
+        if (msg.type === 'api' && msg.content.includes('!combat')) {
+
+            const option = msg.content.replace('!combat ', '');
+
+            switch (option) {
+
+                case 'init':
+
+                if (!playerIsGM(msg.playerid)) return;
+
+                    state.rtnEncounter = {
+
+                        combatRound: 1,
+                        turn: 1,
+                        numOfCombatants: 0,
+                        trackingActive: false
+
+                    };
+
+                    turnOrder = JSON.parse(Campaign().get('turnorder'));
+
+                    if (turnOrder) {
+
+                        turnOrder = null;
+
+                        Campaign().set('turnorder', JSON.stringify(turnOrder));
+
+                        log('RT: Emptied the turn order!');
+
+                    }
+
+                    await sendChat('', `/desc Combat has started!!!\n/desc Roll for initiative!\n[Start Tracking](!combat startTrack)`);
+                    break;
+
+                case 'startTrack':
+                    
+
+                    log(`Is player gm? ${playerIsGM(msg.playerid)}, Player id: ${msg.playerid}`);
+
+                    if(!playerIsGM(msg.playerid)) return;
+
+                    log('Before if empty turnorder query');
+                    turnOrder = JSON.parse(Campaign().get('turnorder'));
+                    log('Got turnOrder obj!');
+                    if (!turnOrder) return sendChat('', '/desc The turn order is empty! Roll for initiative!');
+                    log('After if empty turnorder query');
+
+                    state.rtnEncounter.numOfCombatants = turnOrder.length;
+                    state.rtnEncounter.trackingActive = true;
+
+                    outputRoundInfo(turnOrder);
+                    break;
+
+
+                case 'nextRound':
+
+                    turnOrder = JSON.parse(Campaign().get('turnorder'));
+                    const turnOf = getObj('graphic', turnOrder[state.rtnEncounter.turn - 1].id);
+
+                    if (!state.rtnEncounter.trackingActive || !playerIsGM(msg.playerid) && msg.playerid !== turnOf.get('controlledby')) return;
+
+                    state.rtnEncounter.turn++;
+                    state.rtnEncounter.numOfCombatants = turnOrder.length;
+
+                    if (state.rtnEncounter.numOfCombatants < state.rtnEncounter.turn) {
+
+                        state.rtnEncounter.combatRound++;
+                        state.rtnEncounter.turn = 1;
+
+                    }
+
+                    outputRoundInfo(turnOrder);
+                    break;
+
+
+                case 'stopTrack':
+                    if (!state.rtnEncounter.trackingActive) return;
+                    await sendChat('', '/desc Combat has ended!');
+                    state.rtnEncounter.trackingActive = false;
+                    break;
+
+                default:
+                    return;
+            }
+        }
+    });
+
+    on('change:campaign:turnorder', (obj, prev) => {
+
+        if (obj.get('turnorder') === prev.turnorder) return;
+
+        const turnOrder = JSON.parse(obj.get('turnorder')).sort((a, b) => { 
+            return b.pr - a.pr;
+        });
+
+        obj.set('turnorder', JSON.stringify(turnOrder));
+
+    });
+
+    async function outputRoundInfo(turnOrder) {
+
+        const turnOf = getObj('graphic', turnOrder[state.rtnEncounter.turn - 1].id);
+
+        await sendChat('', `/desc ROUND: ${state.rtnEncounter.combatRound}`);
+        await sendChat('', `/desc It is ${turnOf.get('name')}'s turn\n[Next turn](!combat nextRound) [End Encounter](!combat stopTrack)`);
+
+        sendPing(turnOf.get('left'), turnOf.get('top'), Campaign().get('playerpageid'));
+
+    }
+});
+
+
