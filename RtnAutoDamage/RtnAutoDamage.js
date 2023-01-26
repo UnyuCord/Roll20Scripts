e746a289e492cca5263e237ccc7c10cf9e51917d
Succling <nikecoes@gmail.com>
2023-01-26 14:59:12 +0100

    AutoDamage wip

--- RtnAutoDamage/RtnAutoDamage.js
+++ RtnAutoDamage/RtnAutoDamage.js
@@ -1,30 +1,80 @@
-'use strict';
-
 // Options
-const autoDamageOptions = {
+var RtnAutoDamage = RtnAutoDamage || {};
+
+RtnAutoDamage.Options = {
 
     HEALTH_BAR: 'bar1',
+    // Turn track makes it so that the damage interface is only sent to the player whose turn it is. 
+    // Requires RtnRoundFramework!!!
+    //TODO: Dont forget to turn the default to false after script is done!!!
+    ENABLE_TURN_TRACK: true,
     ANNOUNCE_PLAYER_DAMAGE: true,
     ANNOUNCE_MONSTER_DAMAGE: true,
+}
 
-};
+RtnAutoDamage.AutoDamage = {
+
+    enabled: true,
+    lastDamage: [],
+    lastDamageTypes: [],
+
+}
+
+if (!RtnRoundFramework) {
+
+    RtnAutoDamage.AutoDamage.enabled = false;
+    log('RtnAutoDamage: No RtnRoundFramework found!');
+
+}
 
 on('ready', () => {
 
-    log('RtnAutoDamage ready');
+    log('RT: RtnAutoDamage ready!');
 
 });
 
-on('chat:message', origMsg => {
+on('chat:message', async origMsg => {
+
+    if (!RtnAutoDamage.AutoDamage.enabled) return;
 
     const message = _.clone(origMsg);
 
     if (message.inlinerolls && (/{{dmg\d=/).test(message.content)) {
 
-
+        RtnAutoDamage.AutoDamage.lastDamage, RtnAutoDamage.AutoDamage.lastDamageTypes =  [];
+        
         message.content = extractRoll(message);
-        log();
 
+        const buttons = `[Attack](!autoDamage regularAttack " style="font-size:0px"><img src="https://s3.amazonaws.com/files.d20.io/images/19/Vp6EsSd3aGqqL78ZPmw1WQ/icon.png?1575370629)
+        [CritAttack](!autoDamage critAttack " style="font-size:0px"><img src="https://s3.amazonaws.com/files.d20.io/images/45/WqJ5IsRZ6gqwkicslVkRuQ/icon.png?1575370659)
+        [Heal](!autoDamage heal " style="font-size:0px"><img src="https://s3.amazonaws.com/files.d20.io/images/9/Ocz5uo9OTzzqpVHfOEYqqQ/icon.png?1575370618)`
+
+        await sendChat('', buttons);
+
+
+    } else return;
+
+    if (message.type === 'api' && message.content.includes('!autoDamage')) {
+
+        const command = message.content.replace('!autoDamage ', '');
+
+        switch (command) {
+
+            case 'regularAttack':
+                log('regular Attack');
+                break;
+            
+            case 'critAttack':
+                log('critAttack');
+                break;
+            
+            case 'heal':
+                log('heal');
+                break;
+
+            default: return;
+
+        }
     }
 
     function extractRoll(msg) {
@@ -40,19 +90,25 @@ on('chat:message', origMsg => {
             .value();
     }
 
-    function getValueFromRoll(msg, rollname, isString = 0){
-        const pattern = new RegExp('{{' + rollname + '=(.+?)}}');
-        let result = 0;
-        if (isString > 0) {
-            msg.content.replace(pattern,(match,rollResult)=>{
-                result = rollResult;
-            });
-        } else {
-            msg.content.replace(pattern,(match,rollResult)=>{
-            result = parseInt(rollResult);
-            });
-        }
-        return result;
-    }
+    function getValueFromExtractedRoll(msgContent, rollProperty){
 
+        const pattern = new RegExp('{{' + rollProperty + '=(.+?)}}');
+        let propertyValue;
+
+        msgContent.replace(pattern, (_match, result) => {
+
+            if ((/^\d+$/).test(result)) {
+
+                 propertyValue = parseInt(result);
+
+            } else {
+
+                propertyValue = result;
+
+            } 
+        });
+
+        return propertyValue;
+
+    }
 });
