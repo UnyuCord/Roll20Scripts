8163b7bcd3fdec73830a0b2d9ccc793b816198cc
Nikki <nikecoes@gmail.com>
2023-04-03 06:56:18 +0200

    Modular RtnAutoHealthFlag

--- RtnAutoHealthFlag/RtnAutoHealthFlag.js
+++ RtnAutoHealthFlag/RtnAutoHealthFlag.js
@@ -1,74 +1,102 @@
-on('change:graphic', async function(obj) {
+var RtnHealthFlag = RtnHealthFlag || (function () {
+    'use strict';
 
-  const character = getObj('character', obj.get('_represents'));
-  const type = (character.get('controlledby') === '') ? 'Monster' : 'Player';
+    const HEALTH_BAR = 'bar1';
 
-  if (obj.get('bar1_max') === '') {
+    const updateHealthFlags = async function (graphic) {
 
-      const hpAttributeVal = getAttrByName(character.id, 'hp', 'max');
-      
-      obj.set('bar1_value', hpAttributeVal);
-      obj.set('bar1_max', hpAttributeVal);
+        const character = getObj('character', graphic.get('_represents'));
+        const type = (character.get('controlledby') === '') ? 'Monster' : 'Player';
 
-    }
-   
-    if (obj.get('bar1_value') <= obj.get('bar1_max') / 2 
-        && type === 'Monster') {
+        if (graphic.get(`${HEALTH_BAR}_value`) <= 0) {
+
+            if (graphic.get('status_dead') || graphic.get('status_death-zone')) return;
+
+            if (type === 'Monster') {
+
+                graphic.set({
+                    'status_dead': true,
+                    'status_broken-heart': false
+                });
+
+                return sendChat('', `/desc ${graphic.get('name')} is dead!`);
+
+            } else {
+
+                graphic.set({
+                    'status_death-zone': true
+                });
+
+                return sendChat('', `/desc ${graphic.get('name')} is downed!`);
+
+            }
+
+        } else if(type === 'Player') {
+
+            graphic.set({
+                'status_death-zone': false
+            });
+
+            return;
+            
+        }
 
 
-      if (obj.get('status_broken-heart') === false) {
+        if (graphic.get('bar1_value') <= graphic.get('bar1_max') / 2
+            && type === 'Monster') {
 
-        obj.set({
 
-          'status_broken-heart': true
-  
-        });
+            if (!graphic.get('status_broken-heart')) {
 
-        await sendChat('', `/desc ${obj.get('name')} is bloodied!`);
+                graphic.set({
 
-      }
+                    'status_broken-heart': true,
+                    'status_dead': false
 
-    } else {
+                });
 
-        obj.set({
+                await sendChat('', `/desc ${graphic.get('name')} is bloodied!`);
 
-            'status_broken-heart': false
+            }
+        }
+    };
 
-        });
+    const handleAddGraphic = function (graphic) {
 
-    }
+        if (graphic.get('bar1_max') === '') {
 
-    if (obj.get('bar1_value') <= 0
-      	&& obj.get('status_dead')) {
+            const characterId = getObj('character', graphic.get('_represents')).id;
+            const hpAttributeVal = getAttrByName(characterId, 'hp', 'max');
 
-      if (type === 'Monster') {
+            graphic.set(`${HEALTH_BAR}_max`, hpAttributeVal);
+            graphic.set(`${HEALTH_BAR}_value`, hpAttributeVal);
+            
 
-        obj.set({
-          'status_dead': true,
-          'status_broken-heart': false
-       });
+        }
 
-       await sendChat('', `/desc ${obj.get('name')} is dead!`);
+    };
 
-      } else {
 
-        obj.set({
+    const registerEventHandlers = function () {
 
-          'status_death-zone': true,
-          'status_broken-heart': false
+        on('add:graphic', handleAddGraphic);
+        on('change:graphic', updateHealthFlags);
 
-        });
+    };
 
-        await sendChat('', `/desc ${obj.get('name')} is downed!`);
+    return {
 
-      }
-      
-    } else {
+        registerEventHandlers,
+        updateHealthFlags
 
-      obj.set({
-        'status_dead': false
-      });
+    };
 
-    }
+})();
+
+on('ready', () => {
+
+    RtnHealthFlag.registerEventHandlers();
+
+    log('RT: HealthFlag ready!');
 
 });
