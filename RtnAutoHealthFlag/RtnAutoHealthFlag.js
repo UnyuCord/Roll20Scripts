5480e9a2a64b806731afeddb70eb7e0781cd8420
Nikki <nikecoes@gmail.com>
2023-12-01 11:58:22 +0100

    Added check to prevent blood being created because health value has not been set

--- RtnAutoHealthFlag/RtnAutoHealthFlag.js
+++ RtnAutoHealthFlag/RtnAutoHealthFlag.js
@@ -3,37 +3,37 @@ var RtnHealthFlag = RtnHealthFlag || (function () {
 
     const HEALTH_BAR = 'bar1';
 
-    const updateHealthFlags = async function (graphic) {
+    const updateHealthFlags = async function (token) {
 
-        const character = getObj('character', graphic.get('_represents'));
+        const character = getObj('character', token.get('_represents'));
         const type = (character.get('controlledby') === '') ? 'Monster' : 'Player';
 
-        if (graphic.get(`${HEALTH_BAR}_value`) <= 0) {
+        if (token.get(`${HEALTH_BAR}_value`) <= 0) {
 
-            if (graphic.get('status_dead') || graphic.get('status_death-zone')) return;
+            if (token.get('status_dead') || token.get('status_death-zone')) return;
 
             if (type === 'Monster') {
 
-                graphic.set({
+                token.set({
                     'status_dead': true,
                     'status_broken-heart': false
                 });
 
-                return sendChat('', `/desc ${graphic.get('name')} is dead!`);
+                return sendChat('', `/desc ${token.get('name')} is dead!`);
 
             } else {
 
-                graphic.set({
+                token.set({
                     'status_death-zone': true
                 });
 
-                return sendChat('', `/desc ${graphic.get('name')} is downed!`);
+                return sendChat('', `/desc ${token.get('name')} is downed!`);
 
             }
 
         } else if (type === 'Player') {
 
-            graphic.set({
+            token.set({
                 'status_death-zone': false
             });
 
@@ -42,36 +42,36 @@ var RtnHealthFlag = RtnHealthFlag || (function () {
         }
 
 
-        if (graphic.get('bar1_value') <= graphic.get('bar1_max') / 2
+        if (token.get('bar1_value') <= token.get('bar1_max') / 2
             && type === 'Monster') {
 
 
-            if (!graphic.get('status_broken-heart')) {
+            if (!token.get('status_broken-heart')) {
 
-                graphic.set({
+                token.set({
 
                     'status_broken-heart': true,
                     'status_dead': false
 
                 });
 
-                await sendChat('', `/desc ${graphic.get('name')} is bloodied!`);
+                await sendChat('', `/desc ${token.get('name')} is bloodied!`);
 
             }
         } else {
-            graphic.set({'status_broken-heart': false});
+            token.set({'status_broken-heart': false});
         }
     };
 
-    const handleAddGraphic = function (graphic) {
+    const handleAddGraphic = function (token) {
 
-        if (graphic.get('bar1_max') === '') {
+        if (token.get('bar1_max') === '') {
 
-            const characterId = getObj('character', graphic.get('_represents')).id;
+            const characterId = getObj('character', token.get('_represents')).id;
             const hpAttributeVal = getAttrByName(characterId, 'hp', 'max');
 
-            graphic.set(`${HEALTH_BAR}_max`, hpAttributeVal);
-            graphic.set(`${HEALTH_BAR}_value`, hpAttributeVal);
+            token.set(`${HEALTH_BAR}_value`, hpAttributeVal);
+            token.set(`${HEALTH_BAR}_max`, hpAttributeVal);
             
 
         }
@@ -81,8 +81,8 @@ var RtnHealthFlag = RtnHealthFlag || (function () {
 
     const registerEventHandlers = function () {
 
-        on('add:graphic', handleAddGraphic);
-        on('change:graphic', updateHealthFlags);
+        on('add:token', handleAddGraphic);
+        on('change:token', updateHealthFlags);
 
     };
 
